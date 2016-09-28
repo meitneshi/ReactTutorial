2ae95b5abad682619897349fb1e5b21cd7082260
emasas <external.maxime.bibos@allianz-assistance.com>
2016-09-28 10:07:37 +0200

    STEP 3.5 : Use Statefull components

--- components/App.jsx
+++ components/App.jsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import Header from './Header.jsx';
 import Content from './Content.jsx';
+import TableRow from './TableRow.jsx';
 
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+
+      this.state = {
+         data:[
+            {
+               "id":1,
+               "name":"John",
+               "age":"20"
+            },{
+               "id":2,
+               "name":"Mark",
+               "age":"30"
+            },{
+               "id":3,
+               "name":"Pat",
+               "age":"40"
+            }
+         ],
+         title: "This is a variable title"
+      }
+   }
+
    render() {
       return (
          <div>
             <Header />
             <Content />
+            <table>
+               <tbody>
+                  {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+               </tbody>
+            </table>
          </div>
       );
    }
